ed16875511596951d6a03ae88bace032521b06ed
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-03-30 14:20:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // Site Name
-  SiteName: 'Uptime Status',
+  SiteName: 'Lewds API Status',
 
   // Site URL
   SiteUrl: '/',
@@ -10,14 +10,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // Support both Monitor-Specific and  Read-Only type's Api Key
   ApiKeys: [
-    'm783399407-b5654044806678e2a377de7f',
-    'm783399409-ec794f627ab64b259503f034',
-    'm786883632-b151d0e9e9796886c2b9c390',
-    'm783491939-22f42834a494fc3eadf69059',
+    'ur1193350-169dfb87db439c8cdb8e6073'
   ],
 
   // if shows monitored site's URL
-  ShowLink: false,
+  ShowLink: true,
 
   // Show how many days of log. 
   // API can have 90 days, although free plan says only for 60 days.
@@ -28,11 +25,11 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://51sec.org/'
+      url: 'https://lewds.ga/docs'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/51sec/uptime-status'
+      text: 'Discord',
+      url: 'https://lewds.ga/discord'
     }
   ]
 };
